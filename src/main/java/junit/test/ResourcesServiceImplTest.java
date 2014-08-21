cc863b60c362368503a860c877a203c96a63b986
zyw <zyw090111@163.com>
2014-08-22 01:09:28 +0800

    初始化

---
+++ src/main/java/junit/test/ResourcesServiceImplTest.java
@@ -0,0 +1,15 @@
+package junit.test;
+
+
+public class ResourcesServiceImplTest {
+
+//	@Test
+//	public void test() {
+//		        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-*.xml");  
+//		        ResourcesServiceImpl bean = (ResourcesServiceImpl)ctx.getBean("resourcesService");
+//		        bean.findAll();
+//		        bean.getById("1");
+//		        bean.getUserResources("1");
+//	}
+
+}
